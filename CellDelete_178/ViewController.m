bdec104ae45fdd2198d98328788198e72bc853e4
steve0503 <leehr76@gmail.com>
2014-01-06 17:25:26 +0900

    CellDelete_178
    Done

--- CellDelete_178/ViewController.m
+++ CellDelete_178/ViewController.m
@@ -8,16 +8,50 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#define CELl_ID @"CELL_ID"
+
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>{
+    
+    NSMutableArray *data;
+    
+}
 
 @end
 
 @implementation ViewController
 
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    [data removeObjectAtIndex:indexPath.row];
+    
+    NSArray *rows = [NSArray arrayWithObject:indexPath];
+    [tableView deleteRowsAtIndexPaths:rows withRowAnimation:UITableViewRowAnimationAutomatic];
+    
+    
+    
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    return [data count];
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:CELl_ID];
+    
+    cell.textLabel.text = [data objectAtIndex:indexPath.row];
+    
+    return cell;
+    
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    data = [[NSMutableArray alloc] initWithObjects:@"a",@"b",@"c",@"d",@"e",@"f", nil];
 }
 
 - (void)didReceiveMemoryWarning
